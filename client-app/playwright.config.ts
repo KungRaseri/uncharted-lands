f31c19025e922b401915d448eeecff30a0f5afd2
KungRaseri <countjoshington@gmail.com>
2023-02-14 22:40:27 -0800

    fix playwright dev server issue

--- client-app/playwright.config.ts
+++ client-app/playwright.config.ts
@@ -99,7 +99,7 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run build && npm run preview',
+    command: 'npm run dev',
     port: 3000,
     reuseExistingServer: !process.env.CI
   }
