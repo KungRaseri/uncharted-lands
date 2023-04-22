7950311c3fb1ef39a65ca803705bc4af089a4493
KungRaseri <countjoshington@gmail.com>
2023-04-21 18:21:42 -0700

    update regiongetpayload

--- src/lib/components/game/map/Region.svelte
+++ src/lib/components/game/map/Region.svelte
@@ -2,7 +2,16 @@
 	import type { Prisma } from '@prisma/client';
 	import TileComponent from './Tile.svelte';
 
-	export let region: Prisma.RegionGetPayload<{ include: { tiles: true } }>;
+	export let region: Prisma.RegionGetPayload<{
+		include: {
+			tiles: {
+				include: {
+					Biome: true;
+					Plots: true;
+				};
+			};
+		};
+	}>;
 </script>
 
 {#each region.tiles as tile}
