34552bca8e3460716da0d1d784939c6bb74816a2
KungRaseri <countjoshington@gmail.com>
2023-09-10 17:13:00 -0700

    package updates

--- src/routes/(protected)/game/profile/+page.server.ts
+++ src/routes/(protected)/game/profile/+page.server.ts
@@ -2,7 +2,6 @@ import { db } from '$lib/db';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ locals }) => {
-    console.log(locals)
     const profile = await db.profile.findUnique({
         where: {
             id: locals.account.profile.id
