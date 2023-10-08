82bb838b26c03d81815365b0a906e11c421155f2
KungRaseri <countjoshington@gmail.com>
2023-10-07 21:09:54 -0700

    started work on construction queue for settlements

--- src/lib/components/game/settlements/Structure.svelte
+++ src/lib/components/game/settlements/Structure.svelte
@@ -1,6 +1,29 @@
 <script lang="ts">
-	import type { Structure } from '@prisma/client';
+	import type { SettlementStructure, Structure } from '@prisma/client';
 
 	export let structure: Structure;
+	export let settlementStructures: SettlementStructure[];
+
+	$: hasBuiltStructure = (id: string) =>
+		settlementStructures.find((ss) => ss.structureId == id) != undefined;
 </script>
 
+<div>
+	<img class="btn-icon rounded-full" src={structure.image} alt={structure.name} />
+	<span class="flex-auto">
+		<dt>{structure.name}</dt>
+		<dd>{structure.description}</dd>
+	</span>
+	<span class="badge variant-soft-tertiary">
+		{settlementStructures.find((ss) => ss.structureId == structure.id)?.level ?? 0}
+	</span>
+	<div class="btn-group">
+		<button class="btn-icon-sm variant-soft-success rounded-full mx-0">+</button>
+		<button
+			class="btn-icon-sm rounded-full"
+			class:variant-glass-error={hasBuiltStructure(structure.id)}
+			disabled={!hasBuiltStructure(structure.id)}>-</button
+		>
+	</div>
+</div>
+<hr />
--- src/lib/components/game/settlements/StructuresList.svelte
+++ src/lib/components/game/settlements/StructuresList.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { Prisma, Structure, SettlementStructure } from '@prisma/client';
+	import StructureComponent from './Structure.svelte';
 
 	export let settlementStructures: SettlementStructure[];
 
@@ -8,19 +9,6 @@
 
 <dl class="list-dl p-4">
 	{#each structures as structure}
-		<div>
-			<img class="btn-icon rounded-full" src={structure.image} alt={structure.name} />
-			<span class="flex-auto">
-				<dt>{structure.name}</dt>
-				<dd>{structure.description}</dd>
-			</span>
-			<span class="badge variant-soft-tertiary">
-				{settlementStructures.find((ss) => ss.structureId == structure.id)?.level ?? 0}
-			</span>
-			<div class="btn-group">
-				<button class="btn-icon-sm variant-soft-success rounded-full mx-0">+</button>
-				<button class="btn-icon-sm variant-glass-error rounded-full mx-0">-</button>
-			</div>
-		</div>
+		<StructureComponent {structure} {settlementStructures} />
 	{/each}
 </dl>
